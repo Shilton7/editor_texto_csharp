a008ddf2be6ebddfe2c0e0cca80260dcf52e9a68
shilton <shilton.santiago@animaeducacao.com.br>
2020-10-25 21:48:17 -0300

    Criando Menu da aplicação

--- Program.cs
+++ Program.cs
@@ -6,7 +6,39 @@ namespace editor_texto_csharp
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello Shilton!");
+      Menu();
     }
+
+    static void Menu()
+    {
+      Console.Clear();
+      Console.WriteLine("O que você deseja fazer ?");
+      Console.WriteLine("1 - Abrir um arquivo");
+      Console.WriteLine("2 - Criar um novo arquivo");
+      Console.WriteLine("0 - Sair");
+
+      short option = short.Parse(Console.ReadLine());
+
+      switch (option)
+      {
+        case 0: Sair(); break;
+        case 1: Abrir(); break;
+        case 2: Novo(); break;
+        default: Menu(); break;
+      }
+    }
+    static void Abrir()
+    {
+
+    }
+    static void Novo()
+    {
+
+    }
+    static void Sair()
+    {
+      System.Environment.Exit(0);
+    }
+
   }
 }
